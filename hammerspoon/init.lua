c83d8fe628db33621953d71676da2b2a1e01d341
Blake Dietz <blakedietz@gmail.com>
2016-10-19 07:10:38 -0600

    Add my own flavor of hotkeys to map to my keyboard maestro setup.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,35 +2,24 @@
 -- A global variable for the Hyper Mode
 k = hs.hotkey.modal.new({}, "F17")
 
--- HYPER+L: Open news.google.com in the default browser
-lfun = function()
-  news = "app = Application.currentApplication(); app.includeStandardAdditions = true; app.doShellScript('open http://news.google.com')"
-  hs.osascript.javascript(news)
-  k.triggered = true
-end
-k:bind('', 'l', nil, lfun)
+hotkeys = {
+	{ '1' },
+	{ 'M' },
+	{ 'R' },
+	{ 'W' },
+	{ 'Q' },
+	{ 'O' },
+	{ 'I' },
+	{ 'S' },
+	{ 'C' },
+	{ 'E' },
+	{ 'T' },
+	{ 'X' }
+}
 
--- HYPER+M: Call a pre-defined trigger in Alfred 3
-mfun = function()
-  cmd = "tell application \"Alfred 3\" to run trigger \"emoj\" in workflow \"com.sindresorhus.emoj\" with argument \"\""
-  hs.osascript.applescript(cmd)
-  k.triggered = true
+for i, hotkey in ipairs(hotkeys) do
+	k:bind({}, hotkey[1], nil, function() hs.eventtap.keyStroke({"cmd","alt","shift","ctrl"}, 'm') end)
 end
-k:bind({}, 'm', nil, mfun)
-
--- HYPER+E: Act like ⌃e and move to end of line.
-efun = function()
-  hs.eventtap.keyStroke({'⌃'}, 'e')
-  k.triggered = true
-end
-k:bind({}, 'e', nil, efun)
-
--- HYPER+A: Act like ⌃a and move to beginning of line.
-afun = function()
-  hs.eventtap.keyStroke({'⌃'}, 'a')
-  k.triggered = true
-end
-k:bind({}, 'a', nil, afun)
 
 -- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
 pressedF18 = function()
